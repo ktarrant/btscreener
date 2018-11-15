dd2700b490c044c1c792cfbbd170643d2c407382
Kevin Tarrant <kmtarrant13@gmail.com>
2018-11-15 01:26:33 -0500

    refactor

---
+++ btscreener/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A one-line description or name.
+A longer description that spans multiple lines.  Explain the purpose of the
+file and provide a short list of the key classes/functions it contains.  This
+is the docstring shown when some does 'import foo;foo?' in IPython, so it
+should be reasonably useful and informative.
+"""
+# -----------------------------------------------------------------------------
+# Copyright (c) 2018, Kevin Tarrant (@ktarrant)
+#
+# Distributed under the terms of the MIT License.
+#
+# The full license is in the file LICENSE.txt, distributed with this software.
+#
+# REFERENCES:
+# http://ipython.org/ipython-doc/rel-0.13.2/development/coding_guide.html
+# https://www.python.org/dev/peps/pep-0008/
+# -----------------------------------------------------------------------------

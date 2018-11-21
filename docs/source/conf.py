450c6baf271a980bd4d89aea3460d95097bbb19e
Kevin Tarrant <kmtarrant13@gmail.com>
2018-11-20 19:49:20 -0500

    use correct sphinx version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,21 +12,21 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../'))
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = u'backtrader screener'
-copyright = u'2018, Kevin Tarrant'
-author = u'Kevin Tarrant'
+project = 'backtrader screener'
+copyright = '2018, Kevin Tarrant'
+author = 'Kevin Tarrant'
 
 # The short X.Y version
-version = u''
+version = ''
 # The full version, including alpha/beta/rc tags
-release = u'0.1'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -40,8 +40,7 @@ release = u'0.1'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.todo',
+    'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
@@ -134,8 +133,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'backtraderscreener.tex', u'backtrader screener Documentation',
-     u'Kevin Tarrant', 'manual'),
+    (master_doc, 'backtraderscreener.tex', 'backtrader screener Documentation',
+     'Kevin Tarrant', 'manual'),
 ]
 
 
@@ -144,7 +143,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'backtraderscreener', u'backtrader screener Documentation',
+    (master_doc, 'backtraderscreener', 'backtrader screener Documentation',
      [author], 1)
 ]
 
@@ -155,7 +154,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'backtraderscreener', u'backtrader screener Documentation',
+    (master_doc, 'backtraderscreener', 'backtrader screener Documentation',
      author, 'backtraderscreener', 'One line description of project.',
      'Miscellaneous'),
 ]
@@ -181,7 +180,7 @@ epub_exclude_files = ['search.html']
 
 # -- Extension configuration -------------------------------------------------
 
-# -- Options for todo extension ----------------------------------------------
+# -- Options for intersphinx extension ---------------------------------------
 
-# If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = True
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {'https://docs.python.org/': None}

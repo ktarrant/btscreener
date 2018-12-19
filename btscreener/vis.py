88dfa144658351fce8eca820012ad27623dbe7a3
Kevin Tarrant <kmtarrant13@gmail.com>
2018-12-19 02:23:38 -0500

    add some style and annotations

--- btscreener/vis.py
+++ btscreener/vis.py
@@ -34,6 +34,10 @@ import numpy as np
 # -----------------------------------------------------------------------------
 # GLOBALS
 # -----------------------------------------------------------------------------
+COLOR_BULL_UP = "#1b9164"
+COLOR_BULL_DOWN = "#e87d3d"
+COLOR_BEAR_UP = "#41b2a0"
+COLOR_BEAR_DOWN = "#e44641"
 
 # -----------------------------------------------------------------------------
 # LOCAL UTILITIES
@@ -63,6 +67,34 @@ logger = logging.getLogger(__name__)
 #
 #
 
+
+# HELPER FUNCTIONS -----------------------------------------
+def create_annotations(scan_result, pct_table, level="resistance"):
+    """
+    Args:
+        scan_result (pd.DataFrame): full scan results table
+        pct_table (pd.DataFrame): table of perecentages made from scan_result
+        level (str): which level to place the label
+
+    Returns:
+        list(dict): for use with layout.annotations
+    """
+    ay = -40 if level is "resistance" else 40
+    return [
+        dict(x=x,
+             y=pct_table.loc[x, "close"],
+             xref='x',
+             yref='y',
+             text='breakout ({})'.format(scan_result.loc[x, level]),
+             showarrow=True,
+             arrowhead=3,
+             ax=0,
+             ay=ay,
+             )
+        for x in pct_table.index
+        if not pd.isnull(scan_result.loc[x, "breakout"])
+    ]
+
 # FUNCTION CATEGORY 1 -----------------------------------------
 def create_bar_graph(scan_result):
     """
@@ -84,26 +116,35 @@ def create_bar_graph(scan_result):
     final_bulls = pct_sup[scan_result.trend == 1.0].sort_values(by='close')
     final_bears = pct_res[scan_result.trend == -1.0].sort_values(by='close')
 
-    trace_bulls = go.Candlestick(
+    trace_bulls = go.Ohlc(
                     x=final_bulls.index,
                     open=final_bulls.prev_close,
                     high=final_bulls.resistance,
                     low=[0] * len(final_bulls.index),
                     close=final_bulls.close,
-                    name="bullish")
-    trace_bears = go.Candlestick(
+                    name="bullish",
+                    increasing=dict(line=dict(color=COLOR_BULL_UP)),
+                    decreasing=dict(line=dict(color=COLOR_BULL_DOWN)))
+    trace_bears = go.Ohlc(
                     x=final_bears.index,
                     open=final_bears.prev_close,
                     high=[0] * len(final_bears.index),
                     low=final_bears.support,
                     close=final_bears.close,
-                    name="bearish")
+                    name="bearish",
+                    increasing=dict(line=dict(color=COLOR_BEAR_UP)),
+                    decreasing=dict(line=dict(color=COLOR_BEAR_DOWN)))
+
+    bull_annotations = create_annotations(scan_result, final_bulls)
+    bear_annotations = create_annotations(scan_result, final_bears, level="support")
     layout = go.Layout(
         xaxis=dict(
             rangeslider=dict(
                 visible=False
             )
-        )
+        ),
+        font=dict(family="Overpass", size=12),
+        annotations=bull_annotations+bear_annotations,
     )
 
     data = [trace_bulls, trace_bears]
